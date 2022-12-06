e6774a2cacb8a0838aa744c53b3017effab8571d
rednazer <zthuang@wisc.edu>
2022-12-06 00:14:50 -0600

    test2

--- test.s
+++ test.s
@@ -1,15 +1,16 @@
 bkpt 0x01
 jal test_target
-bkpt 0x03
-halt
-halt
-halt
-halt
-halt
-halt
-halt
+top:
+	bkpt 0x03
+	halt
+
 test_target:
 	bkpt 0x02
-	jmpr r31, 0x00
+	lih r1, 0x0000
+	lih r2, 0x0000
+	lil r1, 0x0001
+	lil r2, 0x0002
+	sub r20, r1, r2 ; 1-2 < 0
+	blzi top
 	bkpt 0x04
 	halt
