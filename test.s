44a71765a84e363451200059db20301bcee89a68
rednazer <zthuang@wisc.edu>
2022-12-05 23:38:03 -0600

    test2

--- test.s
+++ test.s
@@ -1,31 +1,9 @@
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
-nop
 bkpt 0x01
 jal test_target
 bkpt 0x03
-nop
-nop
-nop
-nop
-nop
 halt
-nop
-nop
-nop
-nop
-nop
-nop
-nop
 test_target:
-	nop
-	nop
 	bkpt 0x02
 	jmpr r31, 0x00
-	
+	bkpt 0x04
+	halt
